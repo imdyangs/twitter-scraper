8695820d33d2f2851b2951c0c4b7d8e1433d9d94
imdyangs <iamdyangster@gmail.com>
2017-06-24 22:26:14 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#Twitter Scraper
+Let's see what's poppin' on twitter.
+
+1. npm install
+2. npm start
+3. Success!
---
+++ Scraper.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+var Twit = require("twit");
+
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
+
+console.log("Start Scrape")
